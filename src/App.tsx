03d9ce26f4c3fbb0f45f4537a668249b791635ff
just-noname-bing <danikfedorov41@gmail.com>
2022-01-22 00:59:58 +0200

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import {
+    ArcballControls,
+    DeviceOrientationControls,
+    Environment,
+    FirstPersonControls,
+    FlyControls,
+    MapControls,
+    OrbitControls,
+    TrackballControls,
+} from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Suspense } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Model = () => {
+    const gltf = useLoader(GLTFLoader, "./scene.gltf");
+    return (
+        <>
+            <primitive
+                object={gltf.scene}
+                scale={1 / 15}
+                rotation={[1 / 3, 1 / 2, 0]}
+                position={[-10, 0, -20]}
+            />
+        </>
+    );
+};
+
+export default function App() {
+    return (
+        <div className="App">
+            <Canvas style={{ minHeight: "100vh" }}>
+                <Suspense fallback={null}>
+                    <Model />
+                    <OrbitControls />
+                    <Environment preset="warehouse" background />
+                </Suspense>
+            </Canvas>
+        </div>
+    );
 }
-
-export default App;
